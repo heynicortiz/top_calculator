98d958fc996e9a9a323cccff05ea48a7bce672a0
Nic Ortiz <ortiz.nagare@gmail.com>
2022-12-28 00:42:41 -0500

    updated layout and refactored js

--- script.js
+++ script.js
@@ -1,93 +1,144 @@
-const entering = document.querySelector("#entering");
-const calculating = document.querySelector("#calculating");
+const current = document.querySelector("#current");
+const previous = document.querySelector("#previous");
 const allDigits = Array.from(document.querySelectorAll(".digit"));
+const operatorList = ["+", "-", "x", "÷"];
 
 // all the non-digit buttons
 const clearButton = document.querySelector("#clearButton");
 const deleteButton = document.querySelector("#deleteButton");
+const decimalButton = document.querySelector("#decimalButton");
 const allOperators = Array.from(document.querySelectorAll(".operator"));
-const divideButton = document.querySelector("#divideButton");
-const multiplyButton = document.querySelector("#multiplyButton");
-const subtractButton = document.querySelector("#subtractButton");
-const addButton = document.querySelector("#addButton");
 const equalsButton = document.querySelector("#equalsButton");
 
-
 // initializing basics for calculator
-let operandFirst = 0;
-let operandSecond = 0;
-let operator = "";
+let operandFirst = null;
+let operandSecond = null;
+let operator = null;
 
-allDigits.forEach(each => each.addEventListener("click", () => {
-    entering.textContent === "hello" ? updateScreen(each.textContent) : updateScreen(entering.textContent += each.textContent);
-}));
+// update visuals
+function chgLower(string) {
+    current.textContent = string;
+}
+
+function chgUpper(string) {
+    previous.textContent = string;
+}
 
 clearButton.addEventListener("click", () => {
-    updateScreen("hello");
-    updateCalculating("");
-    operandFirst = 0;
-    operandSecond = 0;
-    operator = "";
+    chgLower("hello");
+    chgUpper("");
+    operandFirst = null;
+    operandSecond = null;
+    operator = null;
+    chgLower("hello");
 });
 
 deleteButton.addEventListener("click", () => {
-    if (entering.textContent !== "hello") {
-        entering.textContent = entering.textContent.slice(0,entering.textContent.length - 1);
-        if (entering.textContent === "" && calculating.textContent === "") {
-            updateScreen("hello");
+    if (current.textContent !== "hello" && current.textContent !== "oh no you don't!") {
+        current.textContent = current.textContent.slice(0,-1);
+        if (current.textContent === "" && previous.textContent === "") {
+            chgLower("hello");
         }
     }
 });
 
-// different operators
-allOperators.forEach(each => each.addEventListener("click", () => {
-    updateOperator(each.textContent);
+allDigits.forEach(each => each.addEventListener("click", () => {
+    addDigit(each.textContent);
 }));
-function updateOperator(selected) {
-    operator = selected;
-    operandFirst = Number(entering.textContent);
-    updateCalculating(operandFirst + operator);
-    updateScreen("");
-    console.log(`Operator is now ${operator}`);
+
+decimalButton.addEventListener("click", () => {
+    if (!current.textContent.includes(".")) {
+        addDigit(".")
+    }
+})
+
+function addDigit(digit) {
+    current.textContent === "hello" || current.textContent === "oh no you don't!" ? chgLower(digit) : chgLower(current.textContent + digit);
 }
 
+// do math
+function add(first, second) {
+    return first + second;
+}
 
-equalsButton.addEventListener("click", () => {
-    switch (operator) {
+function subtract(first, second) {
+    return first - second;
+}
+
+function divide(first, second) {
+    return first / second;
+}
+
+function multiply(first, second) {
+    return first * second;
+}
+
+function operate(first, second, operatorToDo) {
+    let result;
+    first = Number(first);
+    second = Number(second);
+    switch (operatorToDo) {
         case "":
             break;
         case "+":
-            operandSecond = Number(entering.textContent);
-            updateCalculating(calculating.textContent + operandSecond);
-            updateScreen((operandFirst + operandSecond).toString());
+            result = add(first, second);
             break;
         case "-":
-            operandSecond = Number(entering.textContent);
-            updateCalculating(calculating.textContent + operandSecond);
-            updateScreen((operandFirst - operandSecond).toString());
+            result = subtract(first, second);
             break;
         case "x":
-            operandSecond = Number(entering.textContent);
-            updateCalculating(calculating.textContent + operandSecond);
-            updateScreen((operandFirst * operandSecond).toString());
+            result = multiply(first, second);
             break;
         case "÷":
-            operandSecond = Number(entering.textContent);
-            updateCalculating(calculating.textContent + operandSecond);
-            updateScreen((operandFirst / operandSecond).toString());
+            if (second === 0) {
+                clearButton.click();
+                chgLower("oh no you don't!");
+                return;
+            } else {
+                result = divide(first, second);
+            }
             break;
     }
 
-    operandFirst = 0;
-    operandSecond = 0;
-    operator ="";
+    operandFirst = result;
+    operandSecond = null;
+    operator = null;
+
+    return result;
+}
+
+function updateOperator(selectedOperator) {
+    if (current.textContent !== "hello" && current.textContent !== "oh no you don't!") {
+        if (operandFirst == null) {
+            operandFirst = current.textContent;
+            operator = selectedOperator;
+            chgUpper(operandFirst + operator);
+            chgLower("");
+        } else if (operator == null) {
+            operator = selectedOperator;
+            chgUpper(operandFirst + operator);
+            chgLower("");
+        } else {
+            if (operator === "÷" && current.textContent === "0") {
+                clearButton.click();
+                chgLower("oh no you don't!");
+                return;
+            }
+            equalsButton.click();
+            operator = selectedOperator;
+            chgUpper(operandFirst + operator);
+            chgLower("");
+        }
+    }
+}
+allOperators.forEach(each => each.addEventListener("click", () => {
+    updateOperator(each.textContent);
+}))
+
+equalsButton.addEventListener("click", () => {
+    if (current.textContent !== "hello" && current.textContent !== "oh no you don't!") {
+        operandSecond = current.textContent;
+        chgLower("");
+        chgUpper(operate(operandFirst, operandSecond, operator));
+    }
 })
-
-// updates parts of the screen with the given string
-function updateScreen(string) {
-    entering.textContent = string;
-}
-
-function updateCalculating(string) {
-    calculating.textContent = string;
-}
